99798a086b6359e9c9550791b8c018f325fa3960
davidfinch <davidfinch@hotmail.com>
2022-08-05 01:08:58 -0400

    Initial code without pipeline

---
+++ MyFoobar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFoobar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
